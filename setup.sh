be197d357dc2b43cf2c8a5943d3baf3e93d75480
yung-scipio <113138306+yung-scipio@users.noreply.github.com>
2026-09-15 11:06:41 -0700

    Added new applications and services
    Updated package lists and added new applications for setup.

--- setup.sh
+++ setup.sh
@@ -3,34 +3,62 @@
 # Update system and repos for good measure
 sudo pacman -Syu --noconfirm
 
-# Bedrock applications; syncthing and tailscale
+# Bedrock applications; items that make the rest of this script work
 BEDROCK=(syncthing
          tailscale
+         keyd
+         wl-clipboard
+         bitwarden
+         bitwarden-cli
+         rbw
 )
 
 sudo pacman -S --noconfirm "${BEDROCK[@]}"
 
 # Tiling window manager stuff
-TWM=(scroll
+TWM=(sway
+     ly
 )
 
 sudo pacman -S --noconfirm "${TWM[@]}"
 
-# Essential GUI applications
-GUI=(firefox
-    emacs
+# Essential GUI components
+GUI=(cosmic-session
+     flatpak
 )
 
 sudo pacman -S --noconfirm "${GUI[@]}"
 
 # Terminal toolkit
 TERMINAL=(foot
+        fish
         neovim
         imv
+        zellij
+        paru
 )
 
 sudo pacman -S --noconfirm "${TERMINAL[@]}"
 
+# Flatpak applications
+FLATPAKS=(firefox
+          thunderbird
+          obsidian
+          boxes
+          transmission
+          copyq
+          qalculate
+)
+
+flatpak install "${FLATPAKS[@]}"
+
 # Enable and start syncthing service
 systemctl --user enable syncthing.service
 systemctl --user start syncthing.service
+
+# Log into Bitwarden, copy 
+bw login "andrew.evans@mailbox.org"
+
+# Start tailscaled.service
+sudo systemctl start tailscaled
+tailscale login
